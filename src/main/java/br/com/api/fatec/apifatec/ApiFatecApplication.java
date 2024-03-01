35e736ce507996f2ba3d00cba44f6f523681b302
PachecoFatec <fabiano.pacheco@fatec.sp.gov.br>
2024-02-29 22:44:26 -0300

    aula29-02-2024-endpoints

--- src/main/java/br/com/api/fatec/apifatec/ApiFatecApplication.java
+++ src/main/java/br/com/api/fatec/apifatec/ApiFatecApplication.java
@@ -1,10 +1,32 @@
 package br.com.api.fatec.apifatec;
-
+ 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @SpringBootApplication
 public class ApiFatecApplication {
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello world";
+	}
+	@RequestMapping("/hello")
+	String home2() {
+		return "hello world 2";
+	}
+	@RequestMapping("/numero")
+	Integer homenumero() {
+		return  2;
+		
+	}
+	@RequestMapping("/numeroinput/{num}")
+	Integer numero(@PathVariable Integer num) {
+		return  num;
+		
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFatecApplication.class, args);
