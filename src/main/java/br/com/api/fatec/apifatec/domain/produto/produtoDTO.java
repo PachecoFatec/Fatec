ce252044ac19f43cdb78fcb68fa6fc5618e720e7
Fatec <Fatec@lab3-04>
2024-04-11 21:07:41 -0300

    produto

---
+++ src/main/java/br/com/api/fatec/apifatec/domain/produto/produtoDTO.java
@@ -0,0 +1,35 @@
+package br.com.api.fatec.apifatec.domain.produto;
+
+public class produtoDTO {
+	private String nome;
+	private String descricao;
+	private double preco;
+	private int qtde;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public int getQtde() {
+		return qtde;
+	}
+	public void setQtde(int qtde) {
+		this.qtde = qtde;
+	}
+	
+	
+
+}
